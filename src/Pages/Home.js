c37da3d1c9479704ba3f99282223021573a3ea5c
Ronak Saksena <ronak.sksn@gmail.com>
2025-04-18 18:28:57 +0530

    home component update

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -3,7 +3,8 @@ import React from "react";
 export default function Home() {
   return (
     <div>
-      <h1>Home Pahe</h1>
+      <h1>Home Page</h1>
+      <p>Have a nice day</p>
     </div>
   );
 }
